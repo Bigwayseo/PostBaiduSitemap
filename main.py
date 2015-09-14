22983a805762c084659a8b506b32787ec754c90c
Kxrr <hi@kxrr.us>
2015-09-14 14:17:30 +0800

    test git

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from PostBaiduSitemap import Pbs
 
 urlSitemap = '' # Your sitemap url, like 'http://blog.kxrr.us/index.php/sitemap'
-urlPost = '' # Your Baidu sitemap API, like 'http://data.zz.baidu.com/urls?site=blog.kxrr.us&token=xxxxxxxxxxxx'
+urlPost = '' # Your Baidu API, like 'http://data.zz.baidu.com/urls?site=blog.kxrr.us&token=xxxxxxxxxxxx'
 
 if __name__ == '__main__':
     pbs = Pbs(urlSitemap,urlPost)
